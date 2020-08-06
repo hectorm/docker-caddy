c5f937793c32fd6bc8e79da737315a8365e37d10
Héctor Molinero Fernández <hector@molinero.dev>
2020-08-06 20:57:46 +0200

    Added caddy-dns/cloudflare

--- src/go.mod
+++ src/go.mod
@@ -2,5 +2,6 @@ module caddy
 
 require (
 	github.com/caddyserver/caddy/v2 v2.1.1
+	github.com/caddy-dns/cloudflare 5223567e8690d170771ab232c630c0517c5706b6
 	github.com/caddy-dns/lego-deprecated 8284378873307e09c1bb5d4b8cacc46762cdfefe
 )
--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 	// This is where modules get plugged in:
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
+	_ "github.com/caddy-dns/cloudflare"
 	_ "github.com/caddy-dns/lego-deprecated"
 )
 
